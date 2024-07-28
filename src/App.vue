de07aed99b18f6b7cedaf1737306e77eff788bc8
mynh.hiu <apple@192.168.1.6>
2024-07-28 10:12:55 +0700

    init code base

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Layout from './layouts/Layout.vue';
+</script>
+
+<template>
+  <Layout>
+    <template #content>
+      vue app
+    </template>
+  </Layout>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/layouts/Layout.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <slot name="content"></slot>
+</template>
+
+<style scoped lang="scss" scoped></style>
